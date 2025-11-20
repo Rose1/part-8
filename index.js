7e5beb37564744af74a45e6c07fe6e7c65cf8206
Rose1 <rosarioanccoperalta@gmail.com>
2025-11-19 23:36:13 -0500

    GraphQL exercise 8.2 allBooks

--- index.js
+++ index.js
@@ -48,49 +48,49 @@ let books = [
   {
     title: 'Clean Code',
     published: 2008,
-    author: 'Robert Martin',
+    authorName: 'Robert Martin',
     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring']
   },
   {
     title: 'Agile software development',
     published: 2002,
-    author: 'Robert Martin',
+    authorName: 'Robert Martin',
     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
     genres: ['agile', 'patterns', 'design']
   },
   {
     title: 'Refactoring, edition 2',
     published: 2018,
-    author: 'Martin Fowler',
+    authorName: 'Martin Fowler',
     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring']
   },
   {
     title: 'Refactoring to patterns',
     published: 2008,
-    author: 'Joshua Kerievsky',
+    authorName: 'Joshua Kerievsky',
     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring', 'patterns']
   },  
   {
     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
     published: 2012,
-    author: 'Sandi Metz',
+    authorName: 'Sandi Metz',
     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring', 'design']
   },
   {
     title: 'Crime and punishment',
     published: 1866,
-    author: 'Fyodor Dostoevsky',
+    authorName: 'Fyodor Dostoevsky',
     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
     genres: ['classic', 'crime']
   },
   {
     title: 'Demons',
     published: 1872,
-    author: 'Fyodor Dostoevsky',
+    authorName: 'Fyodor Dostoevsky',
     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
     genres: ['classic', 'revolution']
   },
@@ -109,21 +109,27 @@ const typeDefs = gql`
   type Book {
     title: String!
     published: Int!
-    author: Author
+    authorName: Author
     genres: [String!]!
     id: ID!
-
   }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
+  },
+  Book: {
+    authorName: (root) => {
+      return authors.find(a => a.name === root.authorName)
+    }
   }
 }
 
